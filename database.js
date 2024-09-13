0727b8d4099e62ff9d48dac4e804d4b1a6ecd3d5
Charles <HongweiChen38@gmail.com>
2024-09-13 22:11:19 +1000

    Added database operations

--- database.js
+++ database.js
@@ -3,6 +3,28 @@ import { createClient } from '@supabase/supabase-js';
 
 const { SUPABASE_URL, SUPABASE_ANON_KEY } = JSON.parse(fs.readFileSync('db-secrets.json'));
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-// console.log(supabase.from('posts').select('*').throwOnError());
 
-export default supabase;
+const db = {
+	getAllPosts: async function () {
+		const { data } = await supabase.from('posts').select('*');
+		return data;
+	},
+	getAllApprovedPosts: async function () {
+		const { data } = await supabase.from('posts').select('*').is('is_approved', true);
+		return data;
+	},
+	getProfile: async function () {
+		const { data } = await supabase.from('profiles').select('*');
+		return data;
+	},
+	postNewPost: async function (body) {
+		const { data } = await supabase.from('posts').insert([{ body }]).select('*');
+		return data;
+	},
+	approvePost: async function (postId) {
+		const { data } = await supabase.from('posts').update({ is_approved: true }).eq('id', postId);
+		return data;
+	},
+};
+
+export default db;
