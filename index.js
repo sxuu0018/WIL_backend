5124b51312ede3ce657400083820dff5f951b1e0
Charles <HongweiChen38@gmail.com>
2024-09-13 22:23:58 +1000

    Create API endpoints

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ import db from './database.js';
 
 const app = express();
 
-http.createServer(app).listen(3000);
+app.get('/getAllPosts', async (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(await db.getAllPosts());
+});
+
+app.get('/getAllApprovedPosts', async (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(await db.getAllApprovedPosts());
+});
+
+app.get('/getProfile', async (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(await db.getProfile());
+});
+
+app.post('/postNewPost', async (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(await db.postNewPost(req.body.body));
+});
+
+app.post('/approvePost', async (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(await db.approvePost(req.body.postId));
+});
+
+http.createServer(app).listen(5000);
